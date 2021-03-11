47bf63b609b83e303d0f03bf25b3016353c856e7
Michael Lustig <halfjew22@gmail.com>
2021-03-10 21:29:28 -0500

    Interact with Box contract programatically

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,18 @@ module.exports = async function main(callback) {
   try {
     const accounts = await web3.eth.getAccounts();
     console.log(accounts)
-    
+
+
+    const Box = artifacts.require("Box");
+    const box = await Box.deployed();
+
+    await box.store(23);
+
+    const value = await box.retrieve();
+    console.log(`Box value is ${value.toString()}`)
+
+
+
     callback(0)
   } catch (error) {
     console.error(error)
