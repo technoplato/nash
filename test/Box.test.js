e6ddd3e9951bd1cfab3e2f119b32b14d0bc52264
Michael Lustig <halfjew22@gmail.com>
2021-03-10 21:29:28 -0500

    Nodemon test watcher functional

--- test/Box.test.js
+++ test/Box.test.js
@@ -1,15 +1,20 @@
 const {expect} = require('chai')
+const {BN, expectEvent, expectRevert} = require('@openzeppelin/test-helpers')
 
 const Box = artifacts.require('Box')
 
-contract('Box', function() {
+contract('Box', function([owner, other]) {
+
+  const value = new BN('42');
+
   beforeEach(async function() {
-    this.box = await Box.new();
+    this.box = await Box.new({from: owner});
   })
 
   it('retrieve returns a value previously stored', async function() {
-    await this.box.store(442);
+    await this.box.store(value);
+    console.log(value)
 
-    expect((await this.box.retrieve()).toString()).to.equal('42');
+    expect((await this.box.retrieve())).to.be.bignumber.equal(value);
   })
 })
